729e90cb06519947b3a69f11cf7b5ea46db394ed
Lucas S Rocha <132246409+yetzinn@users.noreply.github.com>
2025-04-27 12:41:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById('button')
+
+button.addEventListener('click', (e) => {
+  alert(
+    'Programação é a chave para transformar ideias em soluções. Cada linha de código é um passo em direção à criação de algo novo e útil. Continue aprendendo e evoluindo!'
+  )
+})
